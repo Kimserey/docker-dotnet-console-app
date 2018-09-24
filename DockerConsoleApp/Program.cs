3821885dce012fdd1731d9232a110ad53cdab0bf
Kimserey <kimserey.lam@groomgy.com>
2018-09-24 09:24:04 +0100

    Added dockerfile

--- DockerConsoleApp/Program.cs
+++ DockerConsoleApp/Program.cs
@@ -2,11 +2,13 @@
 
 namespace DockerConsoleApp
 {
+    // https://docs.microsoft.com/en-us/dotnet/core/docker/docker-basics-dotnet-core
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
